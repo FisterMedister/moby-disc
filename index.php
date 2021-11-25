ae39725315b874efdb7a1972482cab9bf31a1bb3
LivKrogshede <74859654+LivKrogshede@users.noreply.github.com>
2021-11-25 12:24:52 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -6,12 +6,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="src/css/style.css">
     <title>Moby Disc - Home</title>
+    <script src="src/js/bot-navigation.js"></script>
 </head>
 
 <body>
     
     <div class="global-wrapper">
-        
+    <div id="content-home" class="hidden">
+            HOME
+        </div>
+        <div id="content-shop" class="hidden">
+            SHOP
+        </div>
+        <div id="content-profile" class="hidden">
+            PROFILE
+        </div>
+
+        <footer>
+            <nav class="bot-nav">
+                <ul class="bot-menu-list">
+                    <li id="btn-home" class="bot-list-item">
+                        <img class="bot-list-item-img" src="src/img/placeholder.svg">
+                    </li>
+                    <li id="btn-shop" class="bot-list-item-text">
+                        SHOP
+                    </li>
+                    <li id="btn-profile" class="bot-list-item">
+                        <img class="bot-list-item-img" src="src/img/placeholder.svg">
+                    </li>
+                </ul>
+            </nav>
+        </footer>
     </div>
 
 </body>
