a2f6d01d66a7a36077c3b41c70162eb0f63f881b
Stefan <stefanjespersen8@gmail.com>
2021-12-01 15:53:30 +0100

    Made front page

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/css/style.css">
+    <title>Moby Disc - Home</title>
+    <script src="src/js/bot-navigation.js"></script>
+</head>
+
+<body>
+    
+    <div class="global-wrapper">
+    <div id="content-home" class="hidden">
+            HOME
+        </div>
+        <div id="content-shop" class="hidden">
+            SHOP
+        </div>
+        <div id="content-profile" class="hidden">
+            PROFILE
+        </div>
+        <div id="content-bundle"></div>
+
+        <footer>
+            <nav class="bot-nav">
+                <ul class="bot-menu-list">
+                    <li id="btn-home" class="bot-list-item">
+                        <img class="bot-list-item-img" src="src/img/placeholder.svg">
+                    </li>
+                    <li id="btn-shop" class="bot-list-item-text">
+                        SHOP
+                    </li>
+                    <li id="btn-profile" class="bot-list-item">
+                        <img class="bot-list-item-img" src="src/img/placeholder.svg">
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    </div>
+
+</body>
+
+</html>
